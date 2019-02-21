81bff71d472c0df89cbe89d9ef94f95680e1d0a6
Luong Duc Tuan Dat <luongductuandat.psa@gmail.com>
2019-02-21 16:36:50 +0700

    fix webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
 
 const outputDirectory = 'dist';
 
