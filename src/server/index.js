d1d288dfc1485596998d868903cbcc7b7decf4d6
Luong Duc Tuan Dat <luongductuandat.psa@gmail.com>
2019-02-21 17:31:46 +0700

    enable f5

--- src/server/index.js
+++ src/server/index.js
@@ -18,13 +18,14 @@ app.use(urlencodedParser);
 app.use(express.static('dist'));
 app.use(cors());
 // for f5 deploy..
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../dist/index.html'), err => {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//   });
-// });
+app.get('/*', (req, res) => {
+  console.log('in f5');
+  res.sendFile(path.join(__dirname, '../../dist/index.html'), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 app.use(jwt());
 // for ReCheck PassWd when Change passwd
 app.use((req, res, next) => {
